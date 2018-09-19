f10181cd632d04e76d394b6589bbb40a5cf6ad4a
404UsernameNotFound404 <37249924+404UsernameNotFound404@users.noreply.github.com>
2018-09-19 09:11:30 -0400

    push

---
+++ Asigment2/src/ClientRequest.java
@@ -0,0 +1,53 @@
+public class ClientRequest {
+
+	
+		public static void main(String [] args) {
+
+	      StoreClientInfo client1 = new StoreClientInfo();
+		  StoreClientInfo client2 = new StoreClientInfo("John's Grocer");
+		  StoreClientInfo client3 = new StoreClientInfo("Mac's", 125); 
+		  StoreClientInfo client4 = new StoreClientInfo("Jefferson Corner Store", true );
+
+		  System.out.println(client1);
+		  System.out.println(client2);
+		  System.out.println(client3);
+		  System.out.println(client4);
+
+		  client1.setName("John's Deli");
+		  client1.placeOrder(400, false, 0);
+
+		  System.out.println("\n" + client1.getName());
+		  System.out.println("\n" + client1.getBalance());
+
+		  client2.placeOrder(920, false, 0);
+		  client3.placeOrder(1200, true, 34);
+		  client4.placeOrder(1640, true, 46); 
+
+		  System.out.println(client1);
+		  System.out.println(client2);
+		  System.out.println(client3);
+		  System.out.println(client4);
+
+		  client1.payment(600);
+		  client2.payment(1400);
+		  client3.payment(800);
+		  client4.payment(200);
+
+		  System.out.println(client1);
+		  System.out.println(client2);
+		  System.out.println(client3);
+		  System.out.println(client4);
+		  
+		  client1.placeOrder(150, true, 23);
+		  client2.placeOrder(350, false, 0);
+		  client3.placeOrder(123.70, false, 0);
+		  client4.placeOrder(550, true, 52);
+
+		  System.out.println(client1);
+		  System.out.println(client2);
+		  System.out.println(client3);
+		  System.out.println(client4);
+	      
+	}
+
+}
