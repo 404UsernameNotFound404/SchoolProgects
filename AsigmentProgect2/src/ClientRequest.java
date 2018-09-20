83fdff4da977a6053acee9d5bbec814cb0d0a25b
404UsernameNotFound404 <37249924+404UsernameNotFound404@users.noreply.github.com>
2018-09-20 16:26:39 -0400

    working on asigment 2

--- AsigmentProgect2/src/ClientRequest.java
+++ AsigmentProgect2/src/ClientRequest.java
@@ -3,67 +3,15 @@ import java.util.Scanner;
 public class ClientRequest {
 
 	public static void main(String[] args) {
-		int menueSelction;
-		getInput input = new getInput();
-		menueSelction = input.getInput();
-		switch(menueSelction)
-		{
-			case 1:
-				 StoreClientInfo client1 = new StoreClientInfo();
-				 System.out.println(client1);
-				 client1.setName("John's Deli");
-				 client1.placeOrder(400, false, 0);
-				 System.out.println("\n" + client1.getName());
-				 System.out.println("\n" + client1.getBalance());
-				 System.out.println(client1);
-				 client1.payment(600);
-				 System.out.println(client1);
-				 client1.placeOrder(150, true, 23);
-				 System.out.println(client1);
-				break;
-			case 2:
-				StoreClientInfo client2 = new StoreClientInfo("John's Grocer");
-				System.out.println(client2);
-				client2.placeOrder(920, false, 0);
-				System.out.println(client2);
-				client2.payment(1400);
-				System.out.println(client2);
-				client2.placeOrder(350, false, 0);
-				System.out.println(client2);
-				break;
-			case 3:
-				StoreClientInfo client3 = new StoreClientInfo("Mac's", 125);
-				System.out.println(client3);
-				client3.placeOrder(1200, true, 34);
-				 System.out.println(client3);
-				 client3.payment(800);
-				 System.out.println(client3);
-				 client3.placeOrder(123.70, false, 0);
-				 System.out.println(client3);
-				break;
-			case 4:
-				StoreClientInfo client4 = new StoreClientInfo("Jefferson Corner Store", true );
-				System.out.println(client4);
-				client4.placeOrder(1640, true, 46); 
-				System.out.println(client4);
-				client4.payment(200);
-				System.out.println(client4);
-				client4.placeOrder(550, true, 52);
-				System.out.println(client4);
-				client4.placeOrder(550, true, 52);
-				System.out.println(client4);
-				break;
-		}
-	     /*
 		 StoreClientInfo client1 = new StoreClientInfo();
 		 StoreClientInfo client2 = new StoreClientInfo("John's Grocer");
-		 StoreClientInfo client3 = new StoreClientInfo("Mac's", 125);
+		 StoreClientInfo client3 = new StoreClientInfo("Mac's", 1250); 
 		 StoreClientInfo client4 = new StoreClientInfo("Jefferson Corner Store", true );
 
-		  //System.out.println(client1);
-		  //System.out.println(client2);
-		  //System.out.println(client3);
-		  //System.out.println(client4);
+		  System.out.println(client1);
+		  System.out.println(client2);
+		  System.out.println(client3);
+		  System.out.println(client4);
 
 		  client1.setName("John's Deli");
 		  client1.placeOrder(400, false, 0);
@@ -71,36 +19,36 @@ public class ClientRequest {
 		  System.out.println("\n" + client1.getName());
 		  System.out.println("\n" + client1.getBalance());
 
-		  //client2.placeOrder(920, false, 0);
-		  //client3.placeOrder(1200, true, 34);
-		  //client4.placeOrder(1640, true, 46); 
+		  client2.placeOrder(920, false, 0);
+		  client3.placeOrder(1200, true, 34);
+		  client4.placeOrder(1640, true, 46); 
 
 		  System.out.println(client1);
-		  //System.out.println(client2);
-		  //System.out.println(client3);
-		  //System.out.println(client4);
-
-		  client1.payment(600);
-		  //client2.payment(1400);
-		  //client3.payment(800);
-		  //client4.payment(200);
-
-		  System.out.println(client1);
-		  //System.out.println(client2);
-		  //System.out.println(client3);
-		  //System.out.println(client4);
+		  System.out.println(client2);
+		  System.out.println(client3);
+		  System.out.println(client4);
 		  
-		  client1.placeOrder(150, true, 23);
-		  //client2.placeOrder(350, false, 0);
-		  //client3.placeOrder(123.70, false, 0);
-		  //client4.placeOrder(550, true, 52);
+		  System.out.println("\n" + client2.getBalance());
+		  client2.addInterest();
+		  System.out.println(client2);
+		  System.out.println();
+		  
+		  client1.payment(600);
+		  client2.payment(400);
+		  client3.payment(800);
+		  client4.payment(200);
+		  System.out.println();
+
+		  
+		  client1.placeOrder(1500, true, 23);
+		  client2.placeOrder(350, false, 0);
+		  client3.placeOrder(123.70, false, 0);
+		  client4.placeOrder(500, true, 52);
+		  System.out.println();
 
 		  System.out.println(client1);
-		 // System.out.println(client2);
-		  //System.out.println(client3);
-		  //System.out.println(client4);
-		   */
-
+		  System.out.println(client2);
+		  System.out.println(client3);
+		  System.out.println(client4);
 	}
-
 }
--- AsigmentProgect2/src/StoreClientInfo.java
+++ AsigmentProgect2/src/StoreClientInfo.java
@@ -4,61 +4,79 @@ public class StoreClientInfo
 	int ADMINFEE = 20;
 	int CREDITLIMIT = 2000;
 	float SHIPWEIGHT = 0.5f;
-	int INTEREST = 15;
+	float INTEREST = 1.15f;
 	String storeName;
 	float balance = 0;
 	float purchase = 0;
 	float weight = 0;
 	boolean delivery;
 	
-	public static final String ANSI_RED = "\u001B[31m";
-	
 	public StoreClientInfo(String storeName) 
 	{
 		this.storeName = storeName;
+		balance = ADMINFEE + balance;
 	}
-	public StoreClientInfo(String storeName,int balance)
+	public StoreClientInfo(String storeName,int orderAmount)
 	{
+		if(orderAmount + balance > CREDITLIMIT)
+		{
+			System.err.println(storeName + ": Error Cost of Order execeds Credit Limit by " + (orderAmount - CREDITLIMIT) + "$");
+		}else
+		{
+			balance = orderAmount + balance;
+			System.out.println(storeName + ": order placed the cost is " + orderAmount + " the balance is now " + balance);
+		}
 		this.storeName = storeName;
-		this.balance = balance;
 	}
 	public StoreClientInfo(String storeName,boolean delivery)
 	{
 		this.storeName = storeName;
 		this.delivery = delivery;
+		balance = ADMINFEE + balance;
 	}
 	public StoreClientInfo()
 	{
-		storeName = "Name Not Found";		
+		storeName = "poop"; 
+		balance = ADMINFEE + balance;
 	}
 	
 	//////////////////////////////////////////////////////////////////////Constructors above 
 	
 	public void placeOrder(double cost,boolean delivery,float weight) //places an order with cost if its delivery and the weight
 	{
-		cost = cost + (SHIPWEIGHT * weight); 
-		if(cost > CREDITLIMIT)
+		delivery = this.delivery;
+		if(delivery)
 		{
-			System.err.println("Error Cost exexceds credit limit by " + (cost - CREDITLIMIT));
+		cost = cost + (SHIPWEIGHT * weight);
+		}
+		if(cost + balance > CREDITLIMIT)
+		{
+			System.err.println(storeName + ": Error Cost exexceds credit limit by " + (cost - CREDITLIMIT));
+			balance = 0;
 		}else
 		{
 			balance = balance + (float) cost; 
 		}
-		System.out.println("Order Placed \nCost: " + cost);
+		System.out.println(storeName + ": Order Placed the cost is " + cost);
 	}
-	
 	public void payment(int amountPayed)
 	{
 		if(balance - amountPayed >= 0)
 		{
-			System.out.println("Amount Payed: " + amountPayed + "\n balance = " + balance + "\n");
 			balance = balance - amountPayed;
+			System.out.println(storeName + ": the amount payed is " + amountPayed + " now the balance is " + balance + "\n");
 		}else
 		{
 			float changeDue = (balance-amountPayed) * -1;
-			System.err.println("ERROR Payment greater then balance \n Change Due: " + changeDue);
+			System.err.println(storeName + ": ERROR Payment greater then balance \n Change Due: " + changeDue);
 		}
-		
+	}
+	public void addInterest()
+	{		
+		float intrest = ((balance * INTEREST) - balance) / 12;
+		balance = balance - intrest;
+		System.out.println(storeName + ": Intrest added " + intrest + "balance is now " + balance);
+		intrest = 0;
 	}
 	///////////////////////////////////////////////////////////////////////seters and getter below
 	public String getName() {
@@ -68,12 +86,12 @@ public class StoreClientInfo
 		this.storeName = storeName;
 	}
 	public String getBalance() {
-		return "Balance For " + storeName + ": " + balance;
+		return storeName + ": Balance For " + storeName + ": " + balance;
 	}
 	@Override
 	 public String toString() {
 	    
-		return "Store Name: " + storeName + "\n" + "Balance Of Store: " + balance + "\n";
+		return storeName + " Balance Of Store " + balance + "\n";
 		
 	  }
 }
